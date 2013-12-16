4d07e0cc7fd546f28acc105c7a5671b47c5ab787
Shuai Li <lishuai918@gmail.com>
2013-12-16 09:46:02 +0800

    Some change to run.sh and Makefile of rawt_http.

--- apps/rawt_http/run.sh
+++ apps/rawt_http/run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-LWIPLIBDIR=../contrib/ports/unix/proj/lib
+LWIPLIBDIR=../../lwip_contrib/contrib/ports/unix/proj/lib
 IOELIBDIR=../ioengine/lib
 
 export LD_LIBRARY_PATH=$LWIPLIBDIR:$IOELIBDIR:$LD_LIBRARY_PATH
 
-
-./rawt_http
-
+perf record -e cycles:u -g ./rawt_http
 
 exit 0
